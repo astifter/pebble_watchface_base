0e8cc94f346666caaa13cceb7e2b235a373221da
Andreas Neustifter <andreas.neustifter@gmail.com>
2015-03-02 22:02:10 +0100

    Removed dirty indicators.

--- hw_handling.h
+++ hw_handling.h
@@ -8,7 +8,6 @@ typedef void (*hardware_changed_callback)(void);
 void bt_state_init(hardware_changed_callback c);
 void bt_state_deinit(void);
 extern char* bt_state_string;
-extern int   bt_state_string_dirty;
 
 // Functions for battery handling.
 void battery_state_init(hardware_changed_callback c);
--- hw_handling_bt.c
+++ hw_handling_bt.c
@@ -8,7 +8,6 @@ static bool               bt_state;
 static stringbuffer       bt_state_sb;
 static hardware_changed_callback bt_state_changed_callback;
 char* bt_state_string;
-int   bt_state_string_dirty;
 
 // The hardware handlers operate after the same principle:
 // - The initializer reads the state directly, uses the handler to update the string description and registers the hw-callback handler.
@@ -27,7 +26,6 @@ static void handle_bt_event(bool connected) {
     }
 
     bt_state_string = bt_state_sb.retval;
-    bt_state_string_dirty = 1;
 
     bt_state_changed_callback();
 }
